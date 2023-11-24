651f9595328f7b62421ca11790964a22d17d960f
Cesar Chen <cesarchen616@outlook.com>
2023-11-24 14:50:48 -0500

    Initial React and Spring Boot

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Customized-Trojan-Attacks-and-Detection
+
+## How to run the code
+### Run React App
+```
+cd frontend
+npm start
+```
+
+You can now view frontend in the browser.
+
+- Local:            http://localhost:3000
+- On Your Network:  http://yourlocalhost:3000
+
+### Run Spring Boot Server
+Maven is required to run the server. 
+Find `backend/server/src/main/java/trojanand/server/ServerApplication.java` and run it using IDE.
+If you prefer to run it using command line, run the following command in `backend/server` directory.
+```
+mvn spring-boot:run
+```
+
+
---
+++ backend/server/src/main/java/trojanand/server/ServerApplication.java
@@ -0,0 +1,24 @@
+package trojanand.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
+
+/**
+ * Spring Boot application starter class.
+ * Excludes certain auto-configurations like DataSource, Security, and Actuator Security
+ * to cater to specific requirements of this application.
+ */
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		SecurityAutoConfiguration.class,
+		ManagementWebSecurityAutoConfiguration.class
+})
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
